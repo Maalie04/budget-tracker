22165beb19f1eff6de0386b8823827e1f7140858
Jamaal Lawson <maaliemaal@gmail.com>
2021-10-04 09:58:35 -0400

    added onerror function

--- public/db.js
+++ public/db.js
@@ -1,13 +1,28 @@
 let db;
-const request = indexesDB.open("budgetdb",1);
+let budgetVersion;
+
+const request = indexesDB.open("Budgetdb" || 21);
 
 request.onupgradeneeded = function (e) {
     console.log('Upgrade needed!!');
-    const db = e.target.result;
-    db.createObjectStore("pending", { autoIncrement: true });
+
+    const db { oldVersion } = e;
+    const newVersion = e.newVersion || db.version;
+
+    console.log('Updated from ${oldVersion} to ${newVersion}');
+
+    db = e.target.result;
+
+    if(db.objectStoreNames.length === 0){
+        db.createObjectStore('BudgetStore', { autoIncrement: true });
+    }
 };
 
-reques.onsuccess = function (e) {
+request.onerror = function(e){
+    console.log('Somethings not right! ${e.target.errorCode}');
+};
+
+request.onsuccess = function (e) {
     db = e.target.result;
 
     if(navigator.onLine){
